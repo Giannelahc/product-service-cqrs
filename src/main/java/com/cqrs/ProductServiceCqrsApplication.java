f5dec03219981cbfc512cf67a6342cdb4db429f2
Giannela Huamani <giannelahc@gmail.com>
2022-09-20 00:06:25 -0500

    base commit

---
+++ src/main/java/com/cqrs/ProductServiceCqrsApplication.java
@@ -0,0 +1,24 @@
+package com.cqrs;
+
+import com.cqrs.command.api.exception.ProductServiceEventsErrorHandler;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceCqrsApplication.class, args);
+	}
+
+	@Autowired
+	public void configure(EventProcessingConfigurer configurer) {
+		configurer.registerListenerInvocationErrorHandler(
+				"product",
+				configuration -> new ProductServiceEventsErrorHandler()
+		);
+	}
+}
+
